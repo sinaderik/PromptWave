586443e8d1bc88cb7e82d251ff49a7cb54eca2ef
Sina <sinaderik7@gmail.com>
2024-10-14 19:50:28 +0330

    Desktop navigation is implemented

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,6 +6,15 @@ import { signIn, signOut, useSession, getProviders } from "next-auth/react"
 import { useState, useEffect } from "react"
 
 const Nav = () => {
+    const isUserLoggedIn = true
+    const [providers, setProviders] = useState(null)
+    useEffect(() => {
+        const setProvider = async () => {
+            const response = await getProviders()
+            setProviders(response)
+        }
+        setProvider()
+    }, [])
     return (
         <nav className="flex-between mb-16 pt-3 w-full">
             <Link href="/" className="flex flex-center gap-2">
@@ -18,6 +27,43 @@ const Nav = () => {
                 />
                 <p className="logo_text">PromptWave</p>
             </Link>
+
+            {/* Desktop Navigation */}
+            <div className="sm:flex hidden">
+                {isUserLoggedIn
+                    ? (
+                        <div className="flex gap-3 md:gap-5">
+                            <Link className="black_btn" href="/create-prompt">
+                                Create Post
+                            </Link>
+                            <button className="outline_btn" onClick={signOut} >Sign out</button>
+                            <Link href="/profile">
+                                <Image
+                                    className="rounded-full"
+                                    src="/assets/images/logo.svg"
+                                    width={37}
+                                    height={37}
+                                    alt="profile-image"
+                                />
+                            </Link>
+                        </div>
+                    ) : (
+                        <>
+                            {providers && Object.values(providers).map(provider => (
+                                <button
+                                    className="black_btn"
+                                    type="button"
+                                    key={provider.name}
+                                    onClick={() => signIn(provider.id)}
+
+                                >
+                                    Sign In
+                                </button>
+                            ))}
+                        </>
+                    )
+                }
+            </div>
         </nav>
     )
 }
