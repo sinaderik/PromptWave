6cee3b62b484b61aa5b446b729e86eb3d9ddb986
Sina <sinaderik7@gmail.com>
2024-10-14 19:05:31 +0330

    home component is created

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,17 @@
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="w-full flex-center flex-col">
+        <h1 className="head_text text-center">
+            Discover and Share
+            <br className="max-md:hidden" />
+            <span className="orange_gradient text-center">AI-powered Prompts</span>
+        </h1>
+        <p className="desc text-center">PromptWave is an open-source AI prompting tool for modern world to
+        discover, create and share creative prompts</p>
+
+        {/* feed */}
+    </section>
   )
 }
 
