b3b7d208c1589455f1445efc130298ad407a5c38
Sina <sinaderik7@gmail.com>
2024-10-15 16:31:40 +0330

    Mobile Navigation is implemented

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -3,11 +3,14 @@
 import Link from "next/link"
 import Image from "next/image"
 import { signIn, signOut, useSession, getProviders } from "next-auth/react"
-import { useState, useEffect } from "react"
+import { useState, useEffect, useRef } from "react"
 
 const Nav = () => {
     const isUserLoggedIn = true
     const [providers, setProviders] = useState(null)
+    const [toggleDropDown, setToggleDropDown] = useState(false)
+    const menuRef = useRef(null);
+
     useEffect(() => {
         const setProvider = async () => {
             const response = await getProviders()
@@ -15,8 +18,23 @@ const Nav = () => {
         }
         setProvider()
     }, [])
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (menuRef.current && !menuRef.current.contains(event.target)) {
+                setToggleDropDown(false);
+            }
+        }
+
+        if (toggleDropDown) {
+            document.addEventListener("mousedown", handleClickOutside);
+        }
+
+        return () => document.removeEventListener("mousedown", handleClickOutside)
+    }, [toggleDropDown])
+
     return (
         <nav className="flex-between mb-16 pt-3 w-full">
+            {/* logo */}
             <Link href="/" className="flex flex-center gap-2">
                 <Image
                     className="object-contain"
@@ -40,7 +58,7 @@ const Nav = () => {
                             <Link href="/profile">
                                 <Image
                                     className="rounded-full"
-                                    src="/assets/images/logo.svg"
+                                    src="/assets/icons/profile2.svg"
                                     width={37}
                                     height={37}
                                     alt="profile-image"
@@ -55,7 +73,6 @@ const Nav = () => {
                                     type="button"
                                     key={provider.name}
                                     onClick={() => signIn(provider.id)}
-
                                 >
                                     Sign In
                                 </button>
@@ -63,6 +80,64 @@ const Nav = () => {
                         </>
                     )
                 }
+            </div>
+
+            {/* Mobile Navigation */}
+            <div className="sm:hidden flex relative">
+                {isUserLoggedIn
+                    ? (
+                        <>
+                            <Image
+                                className="rounded-full"
+                                src="/assets/icons/menu.svg"
+                                width={37}
+                                height={37}
+                                alt="profile-image"
+                                onClick={() => setToggleDropDown(prev => !prev)}
+                            />
+                            {toggleDropDown && (
+                                <div ref={menuRef} className="dropdown">
+                                    <Link
+                                        href="/profile"
+                                        className="dropdown_link"
+                                        onClick={() => setToggleDropDown(false)}
+                                    >
+                                        My Profile
+                                    </Link>
+                                    <Link
+                                        href="/create-prompt"
+                                        className="dropdown_link"
+                                        onClick={() => setToggleDropDown(false)}
+                                    >
+                                        Create Prompt
+                                    </Link>
+                                    <button
+                                    className="w-full mt-5 black_btn"
+                                    onClick={()=>{
+                                        signOut()
+                                        setToggleDropDown(false)
+                                    }}
+                                    >
+                                        Sign Out
+                                    </button>
+                                </div>
+                            )}
+                        </>
+
+                    ) : (
+                        <>
+                            {providers && Object.values(providers).map(provider => (
+                                <button
+                                    className="black_btn"
+                                    type="button"
+                                    key={provider.name}
+                                    onClick={() => signIn(provider.id)}
+                                >
+                                    Sign In
+                                </button>
+                            ))}
+                        </>
+                    )}
             </div>
         </nav>
     )
